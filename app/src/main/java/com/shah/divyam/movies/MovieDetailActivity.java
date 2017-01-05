9d0b6e8b869e4dac61e2849e8491a3837d0a48ff
shahdivyam <divyamshah15@gmail.com>
2017-01-05 10:08:40 +0530

    Detail movie activity

--- app/src/main/java/com/shah/divyam/movies/MovieDetailActivity.java
+++ app/src/main/java/com/shah/divyam/movies/MovieDetailActivity.java
@@ -3,13 +3,11 @@ package com.shah.divyam.movies;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MovieDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_movie_detail);
     }
 }
